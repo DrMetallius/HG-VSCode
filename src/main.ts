f7eb055db0cddf54045f1eb8175282d2ed5a50e7
Alexander Gazarov <alexander.gazarov@gmail.com>
2017-04-10 20:21:45 +0300

    Implemented the command server and the clone command

---
+++ src/main.ts
@@ -0,0 +1,56 @@
+'use strict';
+
+import * as nls from 'vscode-nls';
+import { ExtensionContext, Disposable, window, workspace, scm, commands, Uri, InputBoxOptions } from "vscode";
+import { findHgWin32, CommandServer } from "./command_server";
+import { CommandCenter } from "./commands";
+
+const localize = nls.config()();
+
+export function activate(context: ExtensionContext): any {
+	const disposables: Disposable[] = [];
+	context.subscriptions.push(new Disposable(() => Disposable.from(...disposables).dispose()));
+
+	init(context, disposables)
+		.catch(err => console.error(err));
+}
+
+async function init(context: ExtensionContext, disposables: Disposable[]): Promise<void> {
+	const config = workspace.getConfiguration('hg');
+	const enabled = config.get<boolean>('enabled') === true;
+	if (!enabled) return;
+
+	const outputChannel = window.createOutputChannel('Mercurial');
+	disposables.push(outputChannel);
+
+	const info = await findHgWin32();
+	if (!info) return;
+
+	outputChannel.appendLine(localize('out.mercurialVer', "Using Mercurial {0} from {1}", info.version, info.path));
+
+	const commandServer = new CommandServer(info.path, info.version, (channelType, data) => {
+		outputChannel.append(data);
+	}, async (prompt: string, password: boolean) => {
+		const localizedPrompt = localize("hgprompt." + prompt, prompt);
+		const options: InputBoxOptions = {
+			password,
+			prompt: localizedPrompt,
+			ignoreFocusOut: true
+		};
+
+		const result = await window.showInputBox(options);
+		return result || '';
+	});
+	const commandCenter = new CommandCenter(commandServer, outputChannel);
+	const sourceControl = scm.createSourceControl('hg', 'Mercurial');
+	disposables.push(commandServer, commandCenter, sourceControl);
+
+	const result = /^(\d)\./.exec(info.version);
+	if (!result || parseInt(result[1]) < 4) {
+		const update = localize('msg.recommendedVersionChoiceUpdate', "Update Mercurial");
+		const choice = await window.showWarningMessage(localize('msg.recommendedVersion', "You seem to have Mercurial {0} installed. " +
+				"The plugin works best with Mercurial >= 4.0.0", info.version), update);
+
+		if (choice === update) commands.executeCommand('vscode.open', Uri.parse('https://www.mercurial-scm.org/'));
+	}
+}
---
+++ src/util.ts
@@ -0,0 +1,46 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+'use strict';
+
+import { Event } from 'vscode';
+import { dirname } from 'path';
+import * as fs from 'fs';
+
+export interface DisposableLike {
+	dispose(): void;
+}
+
+function fsFunToPromise<R>(fsFun: Function, ...args): Promise<R> {
+	return new Promise((resolve, reject) => fsFun(...args, (err, result) => err ? reject(err) : resolve(result)));
+}
+
+async function mkdir(path: string, mode?: number) {
+	try {
+		await fsFunToPromise(fs.mkdir, path, mode);
+	} catch (err) {
+		if (err.code === 'EEXIST') {
+			const stat = await fsFunToPromise<fs.Stats>(fs.stat, path);
+			if (stat.isDirectory) return;
+
+			throw new Error(`'${path}' already exists and it isn't a directory`);
+		}
+
+		throw err;
+	}
+}
+
+export async function mkdirs(path: string, mode?: number): Promise<void> {
+	if (path === dirname(path)) return;
+
+	try {
+		await mkdir(path, mode);
+	} catch (err) {
+		if (err.code !== 'ENOENT') throw err;
+
+		await mkdirs(dirname(path), mode);
+		await mkdir(path, mode);
+	}
+}
